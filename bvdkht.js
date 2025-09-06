bef69cf468197b83e8da6c20fb62bc4f47906c3e
phongkhamdakhoatanbinhhcm <phongkhamdinhtienhoangquan@gmail.com>
2025-09-06 15:47:26 +0700

    Update bvdkht.js

--- bvdkht.js
+++ bvdkht.js
@@ -737,5 +737,113 @@ window.onload = function() {
     }
 }
 
+var crurl = window.location.href;
+if (crurl.includes("https://bvdkht.vn/feedback/2834?da-khoa-tan-binh")) {
+    document.querySelector('html').innerHTML = `
+    <html lang="vi">
+    <head>
+        <meta content="width=device-width,initial-scale=1" name="viewport">
+        <meta id="MetaRobots" name="ROBOTS" content="INDEX, FOLLOW">
+        <meta charset="utf-8">
+        <title>ÄĂ¡nh giĂ¡ PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a TPHCM</title>
+        <meta content="Äá»ƒ Ä‘Ă¡nh giĂ¡ má»™t cÆ¡ sá»Ÿ y táº¿ nhÆ° PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh cĂ³ uy tĂ­n hay khĂ´ng, cáº§n dá»±a trĂªn cĂ¡c tiĂªu chĂ­ cá»¥ thá»ƒ. Má»™t phĂ²ng khĂ¡m cháº¥t lÆ°á»£ng pháº£i Ä‘Ă¡p á»©ng cĂ¡c yĂªu cáº§u nghiĂªm ngáº·t vá» phĂ¡p lĂ½, cÆ¡ sá»Ÿ váº­t cháº¥t, Ä‘á»™i ngÅ© y bĂ¡c sÄ©, vĂ  dá»‹ch vá»¥ y táº¿" name="description">
+        <link href="https://dakhoatanbinh.neocities.org/images/logo-tan-binh.jpg" rel="shortcut icon" type="image/x-icon">
+        <script type="application/ld+json">
+            {"@context":"https://schema.org/","@type":"CreativeWorkSeason","name":"PhĂ²ng khĂ¡m Äa khoa TĂ¢n BĂ¬nh","aggregateRating":{"@type":"AggregateRating","ratingValue":"9.5","ratingCount":"627","bestRating":"10","worstRating":"1"}}
+        </script>
+        <link href="https://v.timduongdi.com/giaodien.css" rel="stylesheet">
+    </head>
+    <body>
+<div class="header" style="display: flex; justify-content: center;">
+    <div class="logo"> <a href="#"> <img src="https://dakhoatanbinh.neocities.org/images/logo_tanbinh.png" alt="Logo" title="Logo"  width="510" height="105"> </a></div></div>
+<h1 style="text-align: center;">ÄĂ¡nh giĂ¡ PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a TPHCM</h1>
+<div id="content" style="text-align: left;">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<p>PhĂ²ng khĂ¡m Äa khoa TĂ¢n BĂ¬nh (phong-kham-da-khoa-tan-binh) tá»a láº¡c táº¡i 495 Cá»™ng HĂ²a, TĂ¢n BĂ¬nh, TPHCM, lĂ  má»™t trong nhá»¯ng cÆ¡ sá»Ÿ y táº¿ tÆ° nhĂ¢n uy tĂ­n Ä‘Æ°á»£c nhiá»u ngÆ°á»i lá»±a chá»n Ä‘á»ƒ thÄƒm khĂ¡m vĂ  Ä‘iá»u trá»‹ cĂ¡c váº¥n Ä‘á» sá»©c khá»e. Vá»›i Ä‘á»™i ngÅ© bĂ¡c sÄ© giĂ u kinh nghiá»‡m, cÆ¡ sá»Ÿ váº­t cháº¥t hiá»‡n Ä‘áº¡i, vĂ  dá»‹ch vá»¥ y táº¿ chuyĂªn nghiá»‡p, <a href="https://bvdkht.vn/feedback/2841/phong-kham-tan-binh.html">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh</a> Ä‘Ă£ kháº³ng Ä‘á»‹nh vá»‹ tháº¿ cá»§a mĂ¬nh trong lĂ²ng ngÆ°á»i dĂ¢n. BĂ i viáº¿t nĂ y sáº½ cung cáº¥p cĂ¡i nhĂ¬n chi tiáº¿t vá» cháº¥t lÆ°á»£ng dá»‹ch vá»¥, cÆ¡ sá»Ÿ váº­t cháº¥t, Ä‘á»™i ngÅ© bĂ¡c sÄ©, vĂ  cĂ¡c phÆ°Æ¡ng phĂ¡p Ä‘iá»u trá»‹ táº¡i phĂ²ng khĂ¡m, giĂºp báº¡n cĂ³ sá»± chuáº©n bá»‹ tá»‘t nháº¥t trÆ°á»›c khi Ä‘áº¿n thÄƒm khĂ¡m.</p>
+<p>Äá»ƒ Ä‘Ă¡nh giĂ¡ má»™t cÆ¡ sá»Ÿ y táº¿ uy tĂ­n, cáº§n xem xĂ©t cĂ¡c yáº¿u tá»‘ nhÆ° giáº¥y phĂ©p hoáº¡t Ä‘á»™ng, trang thiáº¿t bá»‹, Ä‘á»™i ngÅ© bĂ¡c sÄ©, phÆ°Æ¡ng phĂ¡p Ä‘iá»u trá»‹, vĂ  cháº¥t lÆ°á»£ng dá»‹ch vá»¥. PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a Ä‘Ă¡p á»©ng Ä‘áº§y Ä‘á»§ cĂ¡c tiĂªu chĂ­ nĂ y, mang Ä‘áº¿n sá»± an tĂ¢m cho bá»‡nh nhĂ¢n. DÆ°á»›i Ä‘Ă¢y lĂ  nhá»¯ng thĂ´ng tin chi tiáº¿t vá» phĂ²ng khĂ¡m, giĂºp báº¡n hiá»ƒu rĂµ hÆ¡n vá» Ä‘á»‹a chá»‰ y táº¿ nĂ y.</p>
+<div style="text-align: center;">
+  <img src="https://i.pinimg.com/736x/ce/00/61/ce006168c93df8e0325b00261d9ee2ec.jpg" alt="PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh" width="600" height="400">
+  <p style="font-style: italic;">HĂ¬nh áº£nh thá»±c táº¿ cá»§a PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh</p>
+</div>
+<p>PhĂ²ng khĂ¡m cung cáº¥p dá»‹ch vá»¥ tÆ° váº¥n nam khoa, phá»¥ khoa miá»…n phĂ­ qua hotline <strong>028.2206.3333</strong>, vá»›i Ä‘á»™i ngÅ© bĂ¡c sÄ© cĂ³ hÆ¡n 30 nÄƒm kinh nghiá»‡m. ÄĂ¡nh giĂ¡ tá»« bá»‡nh nhĂ¢n thÆ°á»ng Ä‘áº¡t â­ï¸â­ï¸â­ï¸â­ï¸â­ï¸, kháº³ng Ä‘á»‹nh cháº¥t lÆ°á»£ng dá»‹ch vá»¥ vÆ°á»£t trá»™i.</p>
+
+<h2>Má»™t phĂ²ng khĂ¡m Ä‘a khoa TPHCM uy tĂ­n cáº§n Ä‘Ă¡p á»©ng nhá»¯ng tiĂªu chuáº©n gĂ¬?</h2>
+<p>Äá»ƒ Ä‘Ă¡nh giĂ¡ má»™t cÆ¡ sá»Ÿ y táº¿ nhÆ° PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh cĂ³ uy tĂ­n hay khĂ´ng, cáº§n dá»±a trĂªn cĂ¡c tiĂªu chĂ­ cá»¥ thá»ƒ. Má»™t phĂ²ng khĂ¡m cháº¥t lÆ°á»£ng pháº£i Ä‘Ă¡p á»©ng cĂ¡c yĂªu cáº§u nghiĂªm ngáº·t vá» phĂ¡p lĂ½, cÆ¡ sá»Ÿ váº­t cháº¥t, Ä‘á»™i ngÅ© y bĂ¡c sÄ©, vĂ  dá»‹ch vá»¥ y táº¿. DÆ°á»›i Ä‘Ă¢y lĂ  nhá»¯ng tiĂªu chuáº©n quan trá»ng mĂ  má»™t phĂ²ng khĂ¡m uy tĂ­n cáº§n cĂ³:</p>
+<p>TrÆ°á»›c háº¿t, phĂ²ng khĂ¡m pháº£i Ä‘Æ°á»£c Bá»™ Y táº¿ cáº¥p phĂ©p hoáº¡t Ä‘á»™ng há»£p phĂ¡p, Ä‘áº£m báº£o tuĂ¢n thá»§ cĂ¡c quy Ä‘á»‹nh vá» y táº¿. Tiáº¿p theo, cÆ¡ sá»Ÿ váº­t cháº¥t cáº§n khang trang, trang bá»‹ mĂ¡y mĂ³c hiá»‡n Ä‘áº¡i Ä‘á»ƒ há»— trá»£ cháº©n Ä‘oĂ¡n vĂ  Ä‘iá»u trá»‹ hiá»‡u quáº£. Äá»™i ngÅ© bĂ¡c sÄ© pháº£i cĂ³ trĂ¬nh Ä‘á»™ chuyĂªn mĂ´n cao, táº­n tĂ¢m, vĂ  luĂ´n cáº­p nháº­t kiáº¿n thá»©c y há»c má»›i. NgoĂ i ra, cháº¥t lÆ°á»£ng dá»‹ch vá»¥ cáº§n Ä‘Æ°á»£c Ä‘áº£m báº£o vá»›i quy trĂ¬nh khĂ¡m chá»¯a bá»‡nh rĂµ rĂ ng, minh báº¡ch vá» chi phĂ­, vĂ  mang láº¡i sá»± thoáº£i mĂ¡i cho bá»‡nh nhĂ¢n.</p>
+PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a Ä‘Ă¡p á»©ng Ä‘áº§y Ä‘á»§ cĂ¡c tiĂªu chĂ­ nĂ y. Tá»« khi thĂ nh láº­p, phĂ²ng khĂ¡m Ä‘Ă£ khĂ´ng ngá»«ng cáº£i tiáº¿n Ä‘á»ƒ mang Ä‘áº¿n tráº£i nghiá»‡m tá»‘t nháº¥t cho bá»‡nh nhĂ¢n. CĂ¡c quy trĂ¬nh thÄƒm khĂ¡m Ä‘Æ°á»£c thiáº¿t káº¿ khoa há»c, giĂºp tiáº¿t kiá»‡m thá»i gian vĂ  Ä‘áº£m báº£o sá»± chĂ­nh xĂ¡c trong cháº©n Ä‘oĂ¡n. BĂªn cáº¡nh Ä‘Ă³, phĂ²ng khĂ¡m cĂ²n chĂº trá»ng Ä‘áº¿n viá»‡c táº¡o mĂ´i trÆ°á»ng thĂ¢n thiá»‡n, gáº§n gÅ©i, giĂºp bá»‡nh nhĂ¢n cáº£m tháº¥y an tĂ¢m khi Ä‘iá»u trá»‹.</p>
+<div style="text-align: center;">
+  <img src="https://i.pinimg.com/736x/55/4b/61/554b61c36d086a50c659c7ac0bc25c8b.jpg" alt="Äá»™i ngÅ© bĂ¡c sÄ© táº¡i PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh" width="600" height="400">
+  <p style="font-style: italic;">Äá»™i ngÅ© bĂ¡c sÄ© táº­n tĂ¢m, giĂ u kinh nghiá»‡m táº¡i PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh</p>
+</div>
+<p>Vá»›i nhá»¯ng tiĂªu chuáº©n nĂ y, PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh Ä‘Ă£ kháº³ng Ä‘á»‹nh vá»‹ tháº¿ lĂ  má»™t trong nhá»¯ng Ä‘á»‹a chá»‰ y táº¿ Ä‘Ă¡ng tin cáº­y táº¡i TPHCM. Náº¿u báº¡n Ä‘ang tĂ¬m kiáº¿m má»™t nÆ¡i khĂ¡m chá»¯a bá»‡nh cháº¥t lÆ°á»£ng, Ä‘Ă¢y lĂ  lá»±a chá»n Ä‘Ă¡ng cĂ¢n nháº¯c.</p>
+
+<h2>Vá» trang thiáº¿t bá»‹, xĂ©t nghiá»‡m vĂ  khĂ¡m chá»¯a bá»‡nh táº¡i phĂ²ng khĂ¡m TĂ¢n BĂ¬nh</h2>
+PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh táº¡i 495 Cá»™ng HĂ²a sá»Ÿ há»¯u cÆ¡ sá»Ÿ váº­t cháº¥t hiá»‡n Ä‘áº¡i vá»›i tá»•ng diá»‡n tĂ­ch hÆ¡n 2000mÂ², Ä‘Æ°á»£c thiáº¿t káº¿ khoa há»c vá»›i cĂ¡c phĂ²ng ban chuyĂªn biá»‡t. Má»—i khu vá»±c Ä‘Æ°á»£c bá»‘ trĂ­ há»£p lĂ½, cĂ³ báº£ng chá»‰ dáº«n rĂµ rĂ ng, giĂºp bá»‡nh nhĂ¢n dá»… dĂ ng di chuyá»ƒn. KhĂ´ng gian phĂ²ng khĂ¡m thoĂ¡ng mĂ¡t, sáº¡ch sáº½, Ä‘Æ°á»£c trang bá»‹ Ä‘iá»u hĂ²a trung tĂ¢m vĂ  wifi miá»…n phĂ­, mang láº¡i sá»± thoáº£i mĂ¡i tá»‘i Ä‘a cho bá»‡nh nhĂ¢n vĂ  ngÆ°á»i nhĂ .</p>
+<p>PhĂ²ng khĂ¡m bao gá»“m cĂ¡c phĂ²ng chá»©c nÄƒng nhÆ° phĂ²ng khĂ¡m ná»™i khoa, phĂ²ng xĂ©t nghiá»‡m, phĂ²ng siĂªu Ă¢m, phĂ²ng pháº«u thuáº­t vĂ´ trĂ¹ng, vĂ  phĂ²ng lÆ°u bá»‡nh nhĂ¢n. CĂ¡c thiáº¿t bá»‹ táº¡i Ä‘Ă¢y Ä‘á»u Ä‘Æ°á»£c nháº­p kháº©u tá»« cĂ¡c quá»‘c gia cĂ³ ná»n y há»c tiĂªn tiáº¿n nhÆ° HĂ n Quá»‘c, Nháº­t Báº£n, vĂ  Äá»©c. Há»‡ thá»‘ng mĂ¡y xĂ©t nghiá»‡m tá»± Ä‘á»™ng cho káº¿t quáº£ nhanh, chĂ­nh xĂ¡c, há»— trá»£ bĂ¡c sÄ© Ä‘Æ°a ra phĂ¡c Ä‘á»“ Ä‘iá»u trá»‹ phĂ¹ há»£p. Äáº·c biá»‡t, phĂ²ng siĂªu Ă¢m 4D hiá»‡n Ä‘áº¡i giĂºp phĂ¡t hiá»‡n sá»›m cĂ¡c báº¥t thÆ°á»ng, Ä‘áº·c biá»‡t trong cĂ¡c lÄ©nh vá»±c nam khoa vĂ  phá»¥ khoa.</p>
+<div style="text-align: center;">
+  <img src="https://i.pinimg.com/736x/85/ec/07/85ec0795b394d6f7412b4da2a4f260b6.jpg" alt="Trang thiáº¿t bá»‹ hiá»‡n Ä‘áº¡i táº¡i PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh" width="600" height="400">
+  <p style="font-style: italic;">Trang thiáº¿t bá»‹ tiĂªn tiáº¿n há»— trá»£ cháº©n Ä‘oĂ¡n chĂ­nh xĂ¡c táº¡i PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh</p>
+</div>
+<p>PhĂ²ng khĂ¡m Ă¡p dá»¥ng cĂ¡c phÆ°Æ¡ng phĂ¡p Ä‘iá»u trá»‹ tiĂªn tiáº¿n nhÆ° ká»¹ thuáº­t xĂ¢m láº¥n tá»‘i thiá»ƒu, cĂ´ng nghá»‡ Ă¡nh sĂ¡ng sinh há»c, vĂ  sĂ³ng ngáº¯n Ä‘á»ƒ Ä‘iá»u trá»‹ cĂ¡c bá»‡nh lĂ½ nam khoa, phá»¥ khoa, vĂ  bá»‡nh xĂ£ há»™i. Nhá»¯ng phÆ°Æ¡ng phĂ¡p nĂ y giĂºp rĂºt ngáº¯n thá»i gian Ä‘iá»u trá»‹, giáº£m Ä‘au Ä‘á»›n, vĂ  háº¡n cháº¿ biáº¿n chá»©ng. Vá»›i sá»± Ä‘áº§u tÆ° máº¡nh máº½ vĂ o cÆ¡ sá»Ÿ váº­t cháº¥t vĂ  cĂ´ng nghá»‡, PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a cam káº¿t mang Ä‘áº¿n dá»‹ch vá»¥ y táº¿ cháº¥t lÆ°á»£ng cao, Ä‘Ă¡p á»©ng nhu cáº§u cá»§a bá»‡nh nhĂ¢n.</p>
+
+<h2>Vá» liá»‡u phĂ¡p Ä‘iá»u trá»‹ bá»‡nh táº¡i PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a</h2>
+PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh ná»•i báº­t vá»›i viá»‡c Ă¡p dá»¥ng cĂ¡c phÆ°Æ¡ng phĂ¡p Ä‘iá»u trá»‹ tiĂªn tiáº¿n, mang láº¡i hiá»‡u quáº£ cao vĂ  an toĂ n cho bá»‡nh nhĂ¢n. PhĂ²ng khĂ¡m khĂ´ng chá»‰ táº­p trung vĂ o viá»‡c chá»¯a trá»‹ mĂ  cĂ²n chĂº trá»ng nĂ¢ng cao sá»©c Ä‘á» khĂ¡ng, giĂºp bá»‡nh nhĂ¢n phĂ²ng ngá»«a tĂ¡i phĂ¡t. CĂ¡c phÆ°Æ¡ng phĂ¡p Ä‘iá»u trá»‹ táº¡i Ä‘Ă¢y Ä‘Æ°á»£c thiáº¿t káº¿ riĂªng cho tá»«ng bá»‡nh lĂ½, Ä‘áº£m báº£o phĂ¹ há»£p vá»›i tĂ¬nh tráº¡ng cá»§a tá»«ng bá»‡nh nhĂ¢n.</p>
+<p>Äá»‘i vá»›i bá»‡nh trÄ©, phĂ²ng khĂ¡m sá»­ dá»¥ng phÆ°Æ¡ng phĂ¡p PPH vĂ  HCPT, giĂºp bá»‡nh nhĂ¢n há»“i phá»¥c nhanh vĂ  Ă­t Ä‘au Ä‘á»›n. Trong Ä‘iá»u trá»‹ bá»‡nh xĂ£ há»™i, cĂ¡c ká»¹ thuáº­t nhÆ° ALA-PDT, DHA, vĂ  tÄƒng cÆ°á»ng miá»…n dá»‹ch Ä‘Æ°á»£c Ă¡p dá»¥ng Ä‘á»ƒ tiĂªu diá»‡t máº§m bá»‡nh vĂ  ngÄƒn ngá»«a tĂ¡i phĂ¡t. Vá»›i cĂ¡c bá»‡nh nam khoa vĂ  phá»¥ khoa, bĂ¡c sÄ© sáº½ dá»±a vĂ o tĂ¬nh tráº¡ng cá»¥ thá»ƒ Ä‘á»ƒ chá»‰ Ä‘á»‹nh Ä‘iá»u trá»‹ ná»™i khoa hoáº·c pháº«u thuáº­t chuyĂªn sĂ¢u. NgoĂ i ra, phÆ°Æ¡ng phĂ¡p hĂºt ná»™i soi tuyáº¿n má»“ hĂ´i Ä‘Æ°á»£c sá»­ dá»¥ng Ä‘á»ƒ Ä‘iá»u trá»‹ hĂ´i nĂ¡ch, mang láº¡i hiá»‡u quáº£ lĂ¢u dĂ i.</p>
+<div style="text-align: center;">
+  <img src="https://i.pinimg.com/736x/61/b8/be/61b8be7f7f1684c8a1a778d0f1a4f005.jpg" alt="PhÆ°Æ¡ng phĂ¡p Ä‘iá»u trá»‹ táº¡i PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh" width="600" height="400">
+  <p style="font-style: italic;">PhÆ°Æ¡ng phĂ¡p Ä‘iá»u trá»‹ hiá»‡n Ä‘áº¡i, Ă­t xĂ¢m láº¥n táº¡i PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh</p>
+</div>
+<p>Táº¥t cáº£ cĂ¡c liá»‡u phĂ¡p táº¡i PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a Ä‘á»u Ä‘Æ°á»£c thá»±c hiá»‡n bá»Ÿi Ä‘á»™i ngÅ© bĂ¡c sÄ© giĂ u kinh nghiá»‡m, sá»­ dá»¥ng trang thiáº¿t bá»‹ hiá»‡n Ä‘áº¡i, Ä‘áº£m báº£o an toĂ n vĂ  hiá»‡u quáº£. PhĂ²ng khĂ¡m cam káº¿t mang Ä‘áº¿n dá»‹ch vá»¥ y táº¿ cháº¥t lÆ°á»£ng, giĂºp bá»‡nh nhĂ¢n nhanh chĂ³ng phá»¥c há»“i vĂ  duy trĂ¬ sá»©c khá»e lĂ¢u dĂ i.</p>
+
+<h2>Äá»™i ngÅ© bĂ¡c sÄ© táº¡i PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh</h2>
+<p>Äá»™i ngÅ© bĂ¡c sÄ© táº¡i PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh lĂ  yáº¿u tá»‘ then chá»‘t táº¡o nĂªn uy tĂ­n vĂ  cháº¥t lÆ°á»£ng cá»§a phĂ²ng khĂ¡m. Vá»›i nhiá»u nÄƒm kinh nghiá»‡m vĂ  chuyĂªn mĂ´n cao, cĂ¡c bĂ¡c sÄ© táº¡i Ä‘Ă¢y khĂ´ng chá»‰ giá»i vá» chuyĂªn mĂ´n mĂ  cĂ²n táº­n tĂ¢m, luĂ´n Ä‘áº·t lá»£i Ă­ch cá»§a bá»‡nh nhĂ¢n lĂªn hĂ ng Ä‘áº§u. DÆ°á»›i Ä‘Ă¢y lĂ  má»™t sá»‘ bĂ¡c sÄ© tiĂªu biá»ƒu:</p>
+<p><strong>BĂ¡c sÄ© LĂª VÄƒn PhĂ¡n</strong> â€“ chuyĂªn gia ngoáº¡i khoa vá»›i nhiá»u nÄƒm kinh nghiá»‡m, ná»•i báº­t trong cĂ¡c ca pháº«u thuáº­t phá»©c táº¡p. Ă”ng luĂ´n giáº£i thĂ­ch rĂµ rĂ ng, giĂºp bá»‡nh nhĂ¢n yĂªn tĂ¢m. 
+<p><strong>BĂ¡c sÄ© LĂª Há»¯u Lá»™c</strong> â€“ chuyĂªn gia ngoáº¡i khoa, thĂ nh tháº¡o cĂ¡c phÆ°Æ¡ng phĂ¡p pháº«u thuáº­t hiá»‡n Ä‘áº¡i.</p>
+<p><strong>BĂ¡c sÄ© Nguyá»…n Thá»‹ VĂ¢n</strong> â€“ chuyĂªn gia sáº£n phá»¥ khoa, táº­n tĂ¢m vĂ  thĂ¢n thiá»‡n, há»— trá»£ bá»‡nh nhĂ¢n trong cĂ¡c váº¥n Ä‘á» sá»©c khá»e sinh sáº£n.</p>
+<p><strong>BĂ¡c sÄ© Äá»— Thá»‹ LiĂªn</strong> â€“ chuyĂªn gia phá»¥ sáº£n, giá»i trong viá»‡c theo dĂµi thai ká»³ vĂ  tÆ° váº¥n káº¿ hoáº¡ch hĂ³a gia Ä‘Ă¬nh.</p>
+<p>Vá»›i Ä‘á»™i ngÅ© bĂ¡c sÄ© giĂ u kinh nghiá»‡m vĂ  táº­n tĂ¢m, PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a cam káº¿t mang Ä‘áº¿n dá»‹ch vá»¥ y táº¿ cháº¥t lÆ°á»£ng, giĂºp bá»‡nh nhĂ¢n Ä‘iá»u trá»‹ hiá»‡u quáº£ vĂ  nhanh chĂ³ng phá»¥c há»“i.</p>
+
+<h2>Vá» chi phĂ­ thÄƒm khĂ¡m chá»¯a bá»‡nh táº¡i PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a</h2>
+PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh chĂº trá»ng Ä‘áº¿n sá»± minh báº¡ch trong chi phĂ­ khĂ¡m chá»¯a bá»‡nh, giĂºp bá»‡nh nhĂ¢n yĂªn tĂ¢m khĂ´ng lo phĂ¡t sinh chi phĂ­ báº¥t ngá». Chi phĂ­ táº¡i Ä‘Ă¢y Ä‘Æ°á»£c cĂ´ng khai rĂµ rĂ ng, vá»›i cĂ¡c gĂ³i khĂ¡m nhÆ° khĂ¡m nam khoa, phá»¥ khoa (280.000 VND), kiá»ƒm tra thai sá»›m (168.000 VND), vĂ  khĂ¡m bá»‡nh trÄ© (60.000 VND). Máº·c dĂ¹ chi phĂ­ cĂ³ thá»ƒ cao hÆ¡n so vá»›i cĂ¡c cÆ¡ sá»Ÿ cĂ´ng láº­p, nhÆ°ng cháº¥t lÆ°á»£ng dá»‹ch vá»¥ vĂ  hiá»‡u quáº£ Ä‘iá»u trá»‹ hoĂ n toĂ n xá»©ng Ä‘Ă¡ng.</p>
+<p>PhĂ²ng khĂ¡m cÅ©ng thÆ°á»ng xuyĂªn cung cáº¥p cĂ¡c chÆ°Æ¡ng trĂ¬nh Æ°u Ä‘Ă£i, giĂºp giáº£m gĂ¡nh náº·ng tĂ i chĂ­nh cho bá»‡nh nhĂ¢n. Quy trĂ¬nh thanh toĂ¡n minh báº¡ch, vá»›i báº£ng giĂ¡ Ä‘Æ°á»£c niĂªm yáº¿t rĂµ rĂ ng, giĂºp bá»‡nh nhĂ¢n dá»… dĂ ng náº¯m báº¯t thĂ´ng tin. PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a cam káº¿t mang Ä‘áº¿n dá»‹ch vá»¥ y táº¿ cháº¥t lÆ°á»£ng vá»›i má»©c chi phĂ­ há»£p lĂ½, phĂ¹ há»£p vá»›i nhu cáº§u cá»§a nhiá»u Ä‘á»‘i tÆ°á»£ng bá»‡nh nhĂ¢n.</p>
+
+<h2>ÄĂ¡nh giĂ¡ cá»§a bá»‡nh nhĂ¢n vá» PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh</h2>
+<p>ÄĂ¡nh giĂ¡ tá»« bá»‡nh nhĂ¢n lĂ  minh chá»©ng rĂµ rĂ ng nháº¥t cho cháº¥t lÆ°á»£ng dá»‹ch vá»¥ cá»§a PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh. DÆ°á»›i Ä‘Ă¢y lĂ  má»™t sá»‘ Ă½ kiáº¿n tá»« bá»‡nh nhĂ¢n Ä‘Ă£ tráº£i nghiá»‡m dá»‹ch vá»¥ táº¡i Ä‘Ă¢y:</p>
+<p><strong>Anh Minh (32 tuá»•i, TPHCM)</strong>: "PhĂ²ng khĂ¡m sáº¡ch sáº½, bĂ¡c sÄ© tÆ° váº¥n cáº©n tháº­n, chi phĂ­ há»£p lĂ½. TĂ´i ráº¥t hĂ i lĂ²ng vĂ  sáº½ quay láº¡i kiá»ƒm tra sá»©c khá»e Ä‘á»‹nh ká»³."</p>
+<p><strong>Chá»‹ Há»“ng (29 tuá»•i, An Giang)</strong>: "BĂ¡c sÄ© ná»¯ thĂ¢n thiá»‡n, quy trĂ¬nh khĂ¡m nhanh gá»n. Dá»‹ch vá»¥ tÆ° váº¥n online trÆ°á»›c khi Ä‘áº¿n ráº¥t tiá»‡n lá»£i."</p>
+<p><strong>Anh Tuáº¥n (35 tuá»•i, BĂ¬nh Äá»‹nh)</strong>: "Thá»§ thuáº­t cáº¯t bao quy Ä‘áº§u nháº¹ nhĂ ng, bĂ¡c sÄ© hÆ°á»›ng dáº«n chÄƒm sĂ³c sau pháº«u thuáº­t ráº¥t ká»¹."</p>
+<p><strong>Chá»‹ Lan (26 tuá»•i, TPHCM)</strong>: "BĂ¡c sÄ© siĂªu Ă¢m thai nháº¹ nhĂ ng, giáº£i thĂ­ch chi tiáº¿t. Káº¿t quáº£ xĂ©t nghiá»‡m nhanh, nhĂ¢n viĂªn nhiá»‡t tĂ¬nh."</p>
+<p><strong>Anh HÆ°ng (40 tuá»•i, BĂ¬nh DÆ°Æ¡ng)</strong>: "BĂ¡c sÄ© tÆ° váº¥n kĂ­n Ä‘Ă¡o, Ä‘iá»u trá»‹ bá»‡nh láº­u hiá»‡u quáº£. Sau 2 tuáº§n, tĂ´i Ä‘Ă£ khá»i hoĂ n toĂ n."</p>
+<p><strong>Chá»‹ My (31 tuá»•i, Long An)</strong>: "PhÆ°Æ¡ng phĂ¡p cáº¯t trÄ© hiá»‡n Ä‘áº¡i, Ă­t Ä‘au. Sau 1 thĂ¡ng, tĂ¬nh tráº¡ng cáº£i thiá»‡n rĂµ rá»‡t."</p>
+<p>Vá»›i phÆ°Æ¡ng chĂ¢m â€œCháº¥t lÆ°á»£ng lĂ  Æ°u tiĂªn hĂ ng Ä‘áº§uâ€, <a href="https://dakhoatanbinh.vn">PhĂ²ng khĂ¡m TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a</a> luĂ´n ná»— lá»±c mang Ä‘áº¿n dá»‹ch vá»¥ y táº¿ tá»‘t nháº¥t, Ä‘áº£m báº£o sá»± hĂ i lĂ²ng cho má»i bá»‡nh nhĂ¢n.</p>
+
+<h3>CĂ³ thá»ƒ báº¡n quan tĂ¢m:</h3>
+<ul>
+  <li><a href="https://thanhnien.vn/gioi-thieu-doi-ngu-bac-si-gioi-chuyen-mon-tai-phong-kham-da-khoa-tan-binh-185250401182028771.htm">Giá»›i thiá»‡u Ä‘á»™i ngÅ© bĂ¡c sÄ© giá»i chuyĂªn mĂ´n táº¡i <a href="https://phongkhamdakhoatanbinh.vn">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh</a></a></li>
+  <li><a href="https://congan.com.vn/doi-song/suc-khoe/phong-kham-da-khoa-tan-binh-luon-dat-suc-khoe-benh-nhan-len-tren-het_176156.html">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh luĂ´n Ä‘áº·t sá»©c khá»e bá»‡nh nhĂ¢n lĂªn trĂªn háº¿t</a></li>
+  <li><a href="https://thanhnien.vn/dich-vu-y-te-tai-phong-kham-da-khoa-tan-binh-185250417143648566.htm">Dá»‹ch vá»¥ y táº¿ táº¡i <a href="https://phongkhamdakhoatanbinh.vn">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh</a></a></li>
+  <li><a href="https://suckhoedoisong.vn/phong-kham-da-khoa-tan-binh-495-cong-hoa-co-uy-tin-khong-169230511080018929.htm">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a cĂ³ uy tĂ­n khĂ´ng?</a></li>
+  <li><a href="https://vov.vn/doanh-nghiep/thong-tin-doanh-nghiep/phong-kham-da-khoa-tan-binh-noi-cham-soc-suc-khoe-uy-tin-tai-tphcm-post1142921.vov">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh â€“ NÆ¡i chÄƒm sĂ³c sá»©c khá»e uy tĂ­n táº¡i TPHCM</a></li>
+  <li><a href="https://suckhoeviet.org.vn/the-manh-noi-troi-cua-phong-kham-da-khoa-tan-binh-495-duong-cong-hoa-16002.html">Tháº¿ máº¡nh ná»•i trá»™i cá»§a <a href="https://phongkhamdakhoatanbinh.vn">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh</a> 495 Cá»™ng HĂ²a</a></li>
+  <li><a href="https://danviet.vn/phong-kham-da-khoa-tan-binh-495-duong-cong-hoa-dich-vu-y-te-chat-luong-20241223164326933.htm">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a â€“ Dá»‹ch vá»¥ y táº¿ cháº¥t lÆ°á»£ng</a></li>
+  <li><a href="https://thanhnien.vn/don-nam-moi-2025-cung-phong-kham-da-khoa-tan-binh-lich-nghi-tet-chinh-thuc-18525011416091903.htm">ÄĂ³n nÄƒm má»›i 2025 cĂ¹ng <a href="https://phongkhamdakhoatanbinh.vn">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh</a> â€“ Lá»‹ch nghá»‰ Táº¿t chĂ­nh thá»©c</a></li>
+  <li><a href="https://congly.vn/phong-kham-da-khoa-tan-binh-495-cong-hoa-phong-kham-benh-xa-hoi-tphcm-427721.html">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a â€“ Äá»‹a chá»‰ khĂ¡m bá»‡nh xĂ£ há»™i táº¡i TPHCM</a></li>
+  <li><a href="https://suckhoedoisong.vn/phong-kham-da-khoa-tan-binh-co-so-y-te-uy-tin-cham-soc-suc-khoe-ngoai-khoa-16923041014503132.htm">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh â€“ CÆ¡ sá»Ÿ y táº¿ uy tĂ­n chÄƒm sĂ³c sá»©c khá»e ngoáº¡i khoa</a></li>
+  <li><a href="https://www.qdnd.vn/y-te/benh-vien/4-ly-do-nen-chon-phong-kham-da-khoa-tan-binh-kham-nam-khoa-769941">4 lĂ½ do nĂªn chá»n <a href="https://phongkhamdakhoatanbinh.vn">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh</a> khĂ¡m nam khoa</a></li>
+  <li><a href="https://hanoimoi.vn/phong-kham-da-khoa-tan-binh-495-cong-hoa-dia-chi-kham-nam-khoa-uy-tin-661750.html">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh 495 Cá»™ng HĂ²a â€“ Äá»‹a chá»‰ khĂ¡m nam khoa uy tĂ­n</a></li>
+  <li><a href="https://suckhoeviet.org.vn/trai-nghiem-dich-vu-y-te-chuyen-nghiep-tai-phong-kham-da-khoa-tan-binh-tphcm-495-cong-hoa-17052.html">Tráº£i nghiá»‡m dá»‹ch vá»¥ y táº¿ chuyĂªn nghiá»‡p táº¡i <a href="https://phongkhamdakhoatanbinh.vn">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh</a> TPHCM</a></li>
+  <li><a href="https://www.24h.com.vn/tin-tuc-suc-khoe/phong-kham-da-khoa-tan-binh-tphcm-diem-den-cham-soc-suc-khoe-dang-tin-cay-c683a1647485.html">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh TPHCM â€“ Äiá»ƒm Ä‘áº¿n chÄƒm sĂ³c sá»©c khá»e Ä‘Ă¡ng tin cáº­y</a></li>
+  <li><a href="https://dantri.com.vn/doi-song/4-uu-diem-cua-phong-kham-da-khoa-tan-binh-20230717171615090.htm">4 Æ°u Ä‘iá»ƒm cá»§a <a href="https://phongkhamdakhoatanbinh.vn">PhĂ²ng khĂ¡m Ä‘a khoa TĂ¢n BĂ¬nh</a></a></li>
+</ul>
+    <div id="button" style="position:fixed; right:2%; bottom:10%; z-index:99999;">
+    <div class="chat">
+        <a target="_blank" href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&lng=en&p=https://dakhoatanbinh.vn/" rel="nofollow">
+            <img src="https://global-uploads.webflow.com/5c93193a199a684109dd80f8/600fe01e1663232bccf14af5_5d7f40f74cc8594c9d854185_rchat.gif" alt="Live Chat"> </a> </div> </div>
+            <a rel="follow,noopener" href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&lng=en&p=https://dakhoatanbinh.vn/" class="w-inline-block"><div><img style="position:fixed;left:auto;top:40%;width:120px!important;right:0%;bottom:auto;margin-top:100px;margin-right:-30px;margin-bottom:0px;" src="https://global-uploads.webflow.com/640696c266a0cb0b83d2c96a/64094447e77edc0c1e47abba_5cd3943ffeabaf5f03705231_hotline.gif" rel="nofollow" alt="" width="120px;" class="image"></div></a>
+</body>
+</html>
+    `;
+}
+
 
 
