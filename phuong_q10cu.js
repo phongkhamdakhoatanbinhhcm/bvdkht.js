9435fa0c25ec3f35c0a7277fb24739fc3f859654
phongkhamdakhoatanbinhhcm <phongkhamdinhtienhoangquan@gmail.com>
2025-10-04 14:44:16 +0700

    Add files via upload

---
+++ phuong_q10cu.js
@@ -0,0 +1,282 @@
+var crurl = window.location.href;
+if (crurl.includes("http://bvdkht.vn/feedback/3081/phong-kham-nam-khoa-phuong-vuon-lai"
+)) {
+    document.querySelector('html').innerHTML = `
+   <html lang="vi">
+    <head>
+        <meta content="width=device-width,initial-scale=1" name="viewport">
+        <meta id="MetaRobots" name="ROBOTS" content="INDEX, FOLLOW">
+        <meta charset="utf-8">
+        <title>Top 7 phòng khám nam khoa phường Vườn Lài nổi tiếng về chất lượng dịch vụ y tế</title>
+        <meta content="Phòng khám nam khoa phường Vườn Lài bao gồm: Phòng Khám Đa Khoa Tân Bình, Bệnh viện Nhân Dân 115, Đa khoa Vạn Hạnh, Bệnh viện quận 10, Bệnh viện Trưng Vương, Đa khoa Việt Đức, Phòng khám BS. Từ Thành Trí Dũng" name="description">
+        <link href="https://dakhoatanbinh.neocities.org/images/logo-tan-binh.jpg" rel="shortcut icon" type="image/x-icon">
+        <script type="application/ld+json">
+            {"@context":"https://schema.org/","@type":"CreativeWorkSeason","name":"Top 7 phòng khám nam khoa phường Vườn Lài nổi tiếng về chất lượng dịch vụ y tế","aggregateRating":{"@type":"AggregateRating","ratingValue":"9.5","ratingCount":"627","bestRating":"10","worstRating":"1"}}
+        </script>
+        <link href="https://v.timduongdi.com/giaodien.css" rel="stylesheet">
+    </head>
+    <body>
+<div class="header" style="display: flex; justify-content: center;">
+    <div class="logo"> <a href="#"> <img src="https://dakhoatanbinh.neocities.org/images/logo_tanbinh.png" alt="Logo" title="Logo"  width="510" height="105"> </a></div></div>
+<h1 style="text-align: center;">Top 7 phòng khám nam khoa phường Vườn Lài nổi tiếng về chất lượng dịch vụ y tế</h1>
+<div id="content" style="text-align: left;">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+
+
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phường Vườn Lài là nơi tập trung nhiều bệnh viện danh tiếng, cung cấp dịch vụ chăm sóc và điều trị chất lượng, giúp bệnh nhân đạt được kết quả tốt nhất. Đặc biệt, khu vực này có danh sách 7 phòng khám nam khoa hàng đầu, mang đến sự hỗ trợ hiệu quả cho nam giới khi cần khám chữa bệnh.</span>
+</p>
+<p style="text-align:center;">
+<a href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&amp;lng=en&amp;p=https://dakhoatanbinh.vn/&amp;r=&amp;rf1=&amp;rf2=&amp;cid=bf8407d2c2e943139eb1ec47a5acbfe0&amp;sid=8fddd1c01b8a40d8acba261ccc25cab7">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091067774f85d9944f1a3be28861e9689c12.jpg">
+</a>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>1. Phòng khám nam khoa phường Vườn Lài - Phòng Khám Đa Khoa Tân Bình</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<a href="https://m.phongkhamdakhoatanbinh.vn/">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng Khám Đa Khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;"> là một trong những địa chỉ uy tín đầu tiên tại phường Vườn Lài dành cho nam giới mà bài viết muốn giới thiệu. Đây là cơ sở y tế tư nhân được đông đảo bệnh nhân đánh giá cao nhờ chất lượng dịch vụ và hiệu quả điều trị.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Tại phòng khám, các bác sĩ chuyên điều trị các vấn đề phổ biến về viêm nhiễm nam khoa (như mụn, sưng đau vùng kín, chảy mủ...), các bệnh lý liên quan đến sinh lý nam (xuất tinh sớm, rối loạn cương dương…), tình trạng vô sinh, bệnh lây qua đường tình dục, cũng như các bệnh về đường tiết niệu.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_202509102a3d337a77331d12151f1bf6990997c5.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng Khám Đa Khoa Tân Bình</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<a href="https://bvtwhuecs2.com/upload/files/dsbvpkttby25.pdf">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng Khám Đa Khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;"> sở hữu đội ngũ bác sĩ giàu kinh nghiệm, từng làm việc tại nhiều bệnh viện lớn ở TP.HCM. Bên cạnh chuyên môn vững vàng, các bác sĩ còn luôn tận tâm, thấu hiểu và chia sẻ những khó khăn mà bệnh nhân nam phải đối mặt. Quy trình khám chữa bệnh cũng được thực hiện nhanh chóng, mang lại sự hài lòng cho người đến khám.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám được thiết kế với mục tiêu đảm bảo sự riêng tư và kín đáo, áp dụng mô hình “1 bác sĩ – 1 bệnh nhân” để hỗ trợ bệnh nhân một cách tận tâm và chu đáo. Đây cũng là cách giúp giảm bớt sự ngại ngùng khi điều trị các vấn đề nhạy cảm. Hơn nữa, mọi thông tin trao đổi, hồ sơ bệnh án và các dữ liệu liên quan đều được bảo mật chặt chẽ, bảo vệ tối đa đời sống riêng tư của người bệnh.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Cơ sở khám nam khoa này hoạt động liên tục tất cả các ngày trong tuần, bao gồm cả thứ Bảy, Chủ Nhật và các ngày Lễ, Tết (từ 8h đến 20h), đáp ứng nhu cầu khám chữa bệnh vào các khung giờ linh hoạt của bệnh nhân. Ngoài ra, nếu đặt lịch khám trước, bệnh nhân sẽ được đội ngũ nhân viên hướng dẫn để nhận mã số khám nhanh chóng, đồng thời có thể chọn bác sĩ nam thăm khám để tạo sự thoải mái hơn trong quá trình điều trị.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Địa chỉ: Số 495 Cộng Hòa, Phường 15 - Quận Tân Bình, TP HCM</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Hotline: 0282 206 3333</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Website:&nbsp;</span>
+<a href="https://dakhoatanbinh.vn">
+<span style="background-color:transparent;color:#1155cc;">
+<u>https://dakhoatanbinh.vn</u>
+</span>
+</a>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>2. Phòng khám nam khoa phường Vườn Lài - Bệnh viện Nhân Dân 115</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám nam khoa tại Bệnh viện 115 là địa chỉ tin cậy dành cho nam giới khi cần khám và điều trị các bệnh lý viêm nhiễm cơ quan sinh dục hoặc những vấn đề nhạy cảm. Bệnh viện áp dụng nhiều kỹ thuật chẩn đoán và điều trị hiện đại, từ những trường hợp đơn giản đến phức tạp, mang lại kết quả khả quan cho nhiều bệnh nhân.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_202509114292b5f9b92baf4b2fe4b9a27a7e06ff.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Bệnh viện Nhân Dân 115</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Đây còn là nơi hội tụ đội ngũ y bác sĩ giàu kinh nghiệm, chuyên môn cao và luôn tận tâm trong việc chăm sóc người bệnh. Nam giới đến thăm khám tại Bệnh viện 115 sẽ cảm thấy yên tâm nhờ vào môi trường sạch sẽ, công tác khử trùng được thực hiện thường xuyên nhằm giảm thiểu mùi khó chịu của bệnh viện. Ngoài ra, mức chi phí khám chữa bệnh tại đây cũng rất ổn định và hợp lý, phù hợp với nhiều đối tượng.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Địa chỉ: Số 527 – Đ. Sư Vạn Hạnh, P.12, Q. 10, TP HCM</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>3. Phòng khám nam khoa phường Vườn Lài - Đa khoa Vạn Hạnh</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bệnh viện Đa khoa Vạn Hạnh là một trong những địa chỉ nam khoa uy tín tại Quận 10 mà nam giới có thể cân nhắc khi cần thăm khám. Với chất lượng dịch vụ tốt, bệnh viện mang lại kết quả tối ưu và nhiều tiện ích nhằm hỗ trợ bệnh nhân hiệu quả. Nhờ vậy, phần lớn người đến đây đều cảm thấy hài lòng với kết quả điều trị.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bệnh viện được xây dựng với quy mô 5 tầng và 135 giường bệnh, đáp ứng nhu cầu khám chữa bệnh nội trú cũng như ngoại trú. Bên cạnh chuyên khoa nam khoa, nơi đây còn cung cấp nhiều dịch vụ khám khác như: sản phụ khoa, răng hàm mặt, tai mũi họng, chỉnh hình, tổng quát, và hỗ trợ sinh sản bằng phương pháp IVF.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_20250911334ee8c8d8be0a94f2c1bdddc360a7c6.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Đa khoa Vạn Hạnh</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Chi phí khám tại bệnh viện được đánh giá là phù hợp với khả năng tài chính của đa số bệnh nhân. Ngoài ra, nơi đây cũng chấp nhận khám bảo hiểm y tế, giúp giảm nhẹ gánh nặng chi phí. Đội ngũ y bác sĩ giàu chuyên môn và tận tâm sẽ luôn đồng hành cùng bệnh nhân, giúp nhanh chóng xác định chính xác tình trạng bệnh lý và áp dụng phương pháp điều trị hiệu quả nhất.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Địa chỉ: Số 72-74, Đường Sư Vạn Hạnh, P.12, Quận 10, TPHCM</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>4. Phòng khám nam khoa phường Vườn Lài - Bệnh viện quận 10</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bệnh viện Quận 10 là một cái tên đáng chú ý trong danh sách các cơ sở khám chữa bệnh nam khoa tại khu vực này. Với phương châm mang lại những dịch vụ chăm sóc sức khỏe chất lượng, bệnh viện không chỉ phục vụ người dân sinh sống và làm việc tại Quận 10 mà còn thu hút nhiều bệnh nhân từ các khu vực lân cận.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_20250911906e0674e7326c0ae4300057d29cf093.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Bệnh viện quận 10</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Đội ngũ y bác sĩ tại đây sở hữu trình độ chuyên môn vượt trội, cùng nhiều năm kinh nghiệm tích lũy từ công tác tại các bệnh viện lớn khác. Ngoài việc đảm bảo tính chuyên nghiệp, họ còn thể hiện sự thấu hiểu tâm lý bệnh nhân, luôn tận tình trong thái độ phục vụ và sẵn sàng hỗ trợ mọi lúc khi người bệnh cần.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Địa chỉ: Số 571,&nbsp; Đường Sư Vạn Hạnh - P. 13, Q.10, TPHCM</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>5. Phòng khám nam khoa phường Vườn Lài - Bệnh viện Trưng Vương</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bệnh viện Trưng Vương ngoài chuyên môn về sản phụ khoa chăm sóc sức khỏe phụ nữ còn nổi bật trong các lĩnh vực khác như: chấn thương chỉnh hình, phẫu thuật tạo hình thẩm mỹ, nội tiết, thận học, nam khoa,... Tất cả các hoạt động của bệnh viện đều tập trung vào mục tiêu đảm bảo an toàn, hiệu quả và nâng cao sự hài lòng của bệnh nhân.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091087c8216ad906ce2b236b750aa9a672f2.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Bệnh viện Trưng Vương</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Hơn nữa, bệnh viện sở hữu đội ngũ y bác sĩ giàu kinh nghiệm, được đào tạo bài bản và tận tâm trong việc chăm sóc người bệnh. Hệ thống trang thiết bị, máy móc tại đây cũng không ngừng được đầu tư, nâng cấp và kiểm định chất lượng để mang lại kết quả nhanh chóng, chính xác và an toàn cho bệnh nhân.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Số 39,&nbsp; Đường Sư Vạn Hạnh, P.3, Quận 10, TPHCM</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>6. Phòng khám nam khoa phường Vườn Lài - Đa khoa Việt Đức</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bệnh viện Đa khoa Việt Đức thuộc danh sách các địa chỉ uy tín tại quận 10 trong lĩnh vực nam khoa, hỗ trợ khám và điều trị nhiều vấn đề sức khỏe cho nam giới. Nơi đây luôn tự hào về sự nỗ lực không ngừng của toàn thể đội ngũ y bác sĩ, mang lại sự tận tâm và chu đáo trong việc phục vụ người bệnh.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025092676483ef2bb35ca4d7f28d87871e3b6d4.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Đa khoa Việt Đức</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Đội ngũ chuyên môn gồm các y bác sĩ và điều dưỡng viên được đánh giá cao nhờ tay nghề vững vàng, kiến thức chuyên sâu và thái độ làm việc chuyên nghiệp. Quy trình khám chữa bệnh cũng được tối ưu hóa với sự hướng dẫn tận tình từ các nhân viên hỗ trợ thủ tục. Đồng thời, mức chi phí bình dân hợp lý càng giúp bệnh nhân cảm thấy yên tâm khi lựa chọn dịch vụ tại đây.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Địa chỉ: Số 39,&nbsp; Đường Sư Vạn Hạnh, P.3, Quận 10, TPHCM</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>7. Phòng khám nam khoa phường Vườn Lài - Phòng khám BS. Từ Thành Trí Dũng</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bác sĩ Dũng được nhiều nam giới đánh giá cao nhờ chuyên môn vững vàng cùng thái độ làm việc tận tâm, chu đáo và luôn vui vẻ khi chăm sóc người bệnh. Bên cạnh đó, phòng khám của bác sĩ còn là lựa chọn hàng đầu của nhiều bệnh nhân thay vì các bệnh viện nam khoa tại Quận 10, bởi sự phục vụ tận tình, với đội ngũ nhân viên hết lòng hỗ trợ từng người bệnh.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025092697fd9899c18246258eff15d943958390.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám BS. Từ Thành Trí Dũng</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám của bác sĩ Dũng cũng được trang bị hệ thống máy móc và thiết bị y tế hiện đại, giúp chẩn đoán chính xác và điều trị hiệu quả. Chi phí khám chữa bệnh tại đây lại rất hợp lý, phù hợp với điều kiện kinh tế của nhiều người.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Địa chỉ: Số 271 – 273, Đ. Vĩnh Viễn, P.5, Quận 10, TP.HCM</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Có 7 phòng khám nam khoa tại phường Vườn Lài được đánh giá cao, mang đến sự lựa chọn đáng tin cậy cho nam giới trong việc chăm sóc sức khỏe và phòng ngừa các bệnh lý nam khoa phức tạp. Nếu cần hỗ trợ từ chuyên viên tư vấn một cách nhanh chóng, bạn chỉ cần nhấn vào Khung Chat bên dưới để nhận tư vấn miễn phí, hoạt động 24/24.</span>
+</p>
+<p style="text-align:center;">
+<a href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&amp;lng=en&amp;p=https://dakhoatanbinh.vn/&amp;r=&amp;rf1=&amp;rf2=&amp;cid=bf8407d2c2e943139eb1ec47a5acbfe0&amp;sid=8fddd1c01b8a40d8acba261ccc25cab7">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091067774f85d9944f1a3be28861e9689c12.jpg">
+</a>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">
+<strong>Xem thêm:&nbsp;</strong>
+</span>
+<a href="https://bvdkht.vn/feedback/2964/phong-kham-nam-khoa-quan-10">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Top 6 phòng khám nam khoa quận 10 khám chữa chất lượng với chi phí phải chăng</u>
+</strong>
+</span>
+</a>
+</p>
+<p style="text-align:justify;">
+<a href="http://bvdkht.vn/feedback/3081/phong-kham-nam-khoa-phuong-dien-hong">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Top 7 phòng khám nam khoa phường Diên Hồng đạt chuẩn y khoa</u>
+</strong>
+</span>
+</a>
+</p>
+<p style="text-align:justify;">
+<a href="http://bvdkht.vn/feedback/3081/phong-kham-nam-khoa-phuong-hoa-hung">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Top 7 phòng khám nam khoa phường Hòa Hưng uy tín giá tốt</u>
+</strong>
+</span>
+</a>
+</p>
+
+
+
+
+
+
+
+
+
+   <div id="button" style="position:fixed; right:2%; bottom:10%; z-index:99999;">
+    <div class="chat">
+        <a target="_blank" href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&lng=en&p=https://dakhoatanbinh.vn/" rel="nofollow">
+            <img src="https://global-uploads.webflow.com/5c93193a199a684109dd80f8/600fe01e1663232bccf14af5_5d7f40f74cc8594c9d854185_rchat.gif" alt="Live Chat"> </a> </div> </div>
+            <a rel="follow,noopener" href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&lng=en&p=https://dakhoatanbinh.vn/" class="w-inline-block"><div><img style="position:fixed;left:auto;top:40%;width:120px!important;right:0%;bottom:auto;margin-top:100px;margin-right:-30px;margin-bottom:0px;" src="https://global-uploads.webflow.com/640696c266a0cb0b83d2c96a/64094447e77edc0c1e47abba_5cd3943ffeabaf5f03705231_hotline.gif" rel="nofollow" alt="" width="120px;" class="image"></div></a>
+
+</body>
+</html>
+    `;
+
+}
