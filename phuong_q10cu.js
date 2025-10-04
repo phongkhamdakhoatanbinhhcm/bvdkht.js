6c8c892ad4f6f9b3f8a01e2cb6a2a9e611fc688c
phongkhamdakhoatanbinhhcm <phongkhamdinhtienhoangquan@gmail.com>
2025-10-04 16:00:06 +0700

    Update phuong_q10cu.js

--- phuong_q10cu.js
+++ phuong_q10cu.js
@@ -558,3 +558,282 @@ if (crurl.includes("https://bvdkht.vn/feedback/3081/phong-kham-nam-khoa-phuong-d
 
 }
 
+
+
+
+var crurl = window.location.href;
+if (crurl.includes("https://bvdkht.vn/feedback/3081/phong-kham-nam-khoa-phuong-hoa-hung"
+)) {
+    document.querySelector('html').innerHTML = `
+   <html lang="vi">
+    <head>
+        <meta content="width=device-width,initial-scale=1" name="viewport">
+        <meta id="MetaRobots" name="ROBOTS" content="INDEX, FOLLOW">
+        <meta charset="utf-8">
+        <title>Top 7 phòng khám nam khoa phường Hòa Hưng uy tín giá tốt</title>
+        <meta content="Phòng khám nam khoa phường Hòa Hưng bao gồm: Phòng Khám Đa Khoa Tân Bình, Bệnh viện Nhân Dân 115, Đa khoa Vạn Hạnh, Bệnh viện quận 10, Bệnh viện Trưng Vương, Đa khoa Việt Đức, Phòng khám BS. Từ Thành Trí Dũng" name="description">
+        <link href="https://dakhoatanbinh.neocities.org/images/logo-tan-binh.jpg" rel="shortcut icon" type="image/x-icon">
+        <script type="application/ld+json">
+            {"@context":"https://schema.org/","@type":"CreativeWorkSeason","name":"Top 7 phòng khám nam khoa phường Hòa Hưng uy tín giá tốt","aggregateRating":{"@type":"AggregateRating","ratingValue":"9.5","ratingCount":"627","bestRating":"10","worstRating":"1"}}
+        </script>
+        <link href="https://v.timduongdi.com/giaodien.css" rel="stylesheet">
+    </head>
+    <body>
+<div class="header" style="display: flex; justify-content: center;">
+    <div class="logo"> <a href="#"> <img src="https://dakhoatanbinh.neocities.org/images/logo_tanbinh.png" alt="Logo" title="Logo"  width="510" height="105"> </a></div></div>
+<h1 style="text-align: center;">Top 7 phòng khám nam khoa phường Hòa Hưng uy tín giá tốt</h1>
+<div id="content" style="text-align: left;">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+
+
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phường Hòa Hưng được biết đến như một khu vực hội tụ nhiều bệnh viện uy tín, nổi bật với chất lượng dịch vụ chăm sóc và điều trị hàng đầu, giúp bệnh nhân đạt được kết quả tối ưu. Đặc biệt, nơi đây còn là điểm đến đáng tin cậy với danh sách 7 phòng khám nam khoa hàng đầu, mang đến những giải pháp hiệu quả, đáp ứng nhu cầu chăm sóc sức khỏe cho nam giới.</span>
+</p>
+<p style="text-align:center;">
+<a href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&amp;lng=en&amp;p=https://dakhoatanbinh.vn/&amp;r=&amp;rf1=&amp;rf2=&amp;cid=bf8407d2c2e943139eb1ec47a5acbfe0&amp;sid=8fddd1c01b8a40d8acba261ccc25cab7">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091067774f85d9944f1a3be28861e9689c12.jpg">
+</a>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>1. Phòng khám nam khoa phường Hòa Hưng - Phòng Khám Đa Khoa Tân Bình</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<a href="https://thanhnien.vn/dich-vu-y-te-tai-phong-kham-da-khoa-tan-binh-185250417143648566.htm">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng Khám Đa Khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;"> là một địa chỉ y tế uy tín và đáng tin cậy phục vụ nam giới tại khu vực phường Hòa Hưng, được đông đảo bệnh nhân đánh giá cao nhờ chất lượng dịch vụ vượt trội và hiệu quả điều trị tốt. Đây là một cơ sở tư nhân nổi bật, chuyên cung cấp các dịch vụ sức khỏe nam khoa, tập trung vào những vấn đề thường gặp như viêm nhiễm (mụn nhọt, sưng đau, chảy mủ…), rối loạn chức năng sinh lý (xuất tinh sớm, rối loạn cương dương), điều trị vô sinh, phòng ngừa bệnh lây qua đường tình dục, cũng như chăm sóc các bệnh liên quan đến đường tiết niệu.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám tự hào sở hữu đội ngũ bác sĩ giàu kinh nghiệm, từng làm việc tại các bệnh viện lớn ở TP.HCM. Không chỉ giỏi chuyên môn, các bác sĩ còn được bệnh nhân yêu mến nhờ lòng tận tâm, sự thấu hiểu và khả năng đồng cảm với những nỗi lo mà nam giới thường gặp phải. Quy trình khám chữa bệnh tại đây được xây dựng bài bản, hướng đến sự nhanh chóng và tiện lợi nhằm tối ưu hóa trải nghiệm cho người bệnh.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_20250922353adc41a0cb542cd399faf0632e250a.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng Khám Đa Khoa Tân Bình</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Không gian phòng khám được thiết kế với tiêu chí riêng tư và kín đáo. Mô hình “1 bác sĩ – 1 bệnh nhân” giúp đảm bảo mọi người đều nhận được sự chăm sóc tận tình và chu đáo, đồng thời giảm bớt cảm giác e ngại khi điều trị các vấn đề nhạy cảm. Hơn thế nữa, thông tin và dữ liệu của bệnh nhân luôn được bảo mật nghiêm ngặt, mang lại sự yên tâm tuyệt đối.</span>
+</p>
+<p style="text-align:justify;">
+<a href="https://bvtwhue.com.vn/upload/files/10-phong-kham-da-khoa-tan-binh.pdf">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Phòng Khám Đa Khoa Tân Bình</u>
+</strong>
+</span>
+</a>
+<span style="background-color:transparent;color:#000000;"> hoạt động liên tục cả tuần, bao gồm các ngày thứ Bảy, Chủ Nhật cũng như Lễ, Tết, với thời gian làm việc từ 8h sáng đến 20h tối. Lịch trình linh hoạt này đáp ứng nhu cầu khám chữa bệnh ngoài giờ hành chính, tạo điều kiện thuận lợi cho mọi người. Đặc biệt, khi đặt lịch trước, bệnh nhân sẽ được hỗ trợ nhanh chóng, nhận mã số khám và chọn bác sĩ nam để tăng thêm sự thoải mái trong quá trình điều trị.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Địa chỉ: Số 495 Cộng Hòa, Phường 15, Quận Tân Bình, TP HCM</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Hotline: 0282 206 3333</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Website:&nbsp;</span>
+<a href="https://dakhoatanbinh.vn">
+<span style="background-color:transparent;color:#1155cc;">
+<u>https://dakhoatanbinh.vn</u>
+</span>
+</a>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>2. Phòng khám nam khoa phường Hòa Hưng - Bệnh viện Nhân Dân 115</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám nam khoa tại Bệnh viện 115 từ lâu đã trở thành địa chỉ đáng tin cậy của nhiều nam giới khi cần thăm khám và điều trị các vấn đề liên quan đến viêm nhiễm cơ quan sinh dục hoặc các bệnh lý nhạy cảm. Với việc áp dụng các kỹ thuật chẩn đoán và điều trị tiên tiến, phòng khám không chỉ giải quyết các trường hợp đơn giản mà còn mang lại hiệu quả tích cực cho những ca bệnh phức tạp.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025092697fd9899c18246258eff15d943958390.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Bệnh viện Nhân Dân 115</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Điểm mạnh của phòng khám còn nằm ở đội ngũ y bác sĩ giàu kinh nghiệm, chuyên môn vững vàng và luôn tận tâm trong việc chăm sóc sức khỏe cho người bệnh. Khi đến đây, bệnh nhân sẽ cảm thấy an tâm nhờ vào môi trường khám chữa bệnh rộng rãi, sạch sẽ và được khử trùng thường xuyên để đảm bảo an toàn tối đa. Chi phí thăm khám cũng được duy trì ở mức hợp lý và ổn định, phù hợp với nhiều đối tượng khác nhau, góp phần gia tăng sự hài lòng của người bệnh.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Địa chỉ: Số 527 – Đ. Sư Vạn Hạnh, P.12, Q. 10, TP HCM</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>3. Phòng khám nam khoa phường Hòa Hưng - Đa khoa Vạn Hạnh</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bệnh viện Đa khoa Vạn Hạnh là một cơ sở y tế uy tín về nam khoa tại Quận 10, được đông đảo nam giới tin tưởng lựa chọn khi cần tư vấn và điều trị. Với dịch vụ chuyên nghiệp, bệnh viện không chỉ cam kết mang đến kết quả điều trị tối ưu mà còn cung cấp nhiều tiện ích nhằm hỗ trợ bệnh nhân một cách toàn diện. Nhờ đó, hầu hết bệnh nhân từng đến đây đều bày tỏ sự hài lòng với trải nghiệm khám chữa bệnh và hiệu quả đạt được.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bệnh viện sở hữu quy mô 5 tầng, với tổng cộng 135 giường bệnh, đáp ứng tốt nhu cầu khám chữa bệnh nội trú lẫn ngoại trú. Bên cạnh chuyên môn về nam khoa, bệnh viện còn cung ứng nhiều dịch vụ y tế khác như sản phụ khoa, răng hàm mặt, tai mũi họng, chỉnh hình, khám tổng quát và đặc biệt là hỗ trợ sinh sản bằng kỹ thuật thụ tinh trong ống nghiệm (IVF).</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_20250926481e5d56bd74d10171e459bcbd33f900.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Đa khoa Vạn Hạnh</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Chi phí khám chữa bệnh tại bệnh viện được nhận xét là hợp lý, đáp ứng tốt với điều kiện kinh tế của đa số bệnh nhân. Đặc biệt, việc áp dụng khám chữa bệnh qua bảo hiểm y tế tại đây đã hỗ trợ giảm đáng kể gánh nặng tài chính. Đội ngũ y bác sĩ giàu chuyên môn và tận tâm luôn sẵn sàng đồng hành, đảm bảo chẩn đoán tình trạng bệnh chính xác và đề xuất các phương pháp điều trị hiệu quả nhất.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Địa chỉ: Số 72-74, Đường Sư Vạn Hạnh, P.12, Quận 10, TPHCM</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>4. Phòng khám nam khoa phường Hòa Hưng - Bệnh viện quận 10</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bệnh viện Quận 10 được đánh giá là một trong những cơ sở hàng đầu về khám và điều trị nam khoa tại khu vực. Với mục tiêu mang đến dịch vụ chăm sóc sức khỏe chất lượng cao, bệnh viện không chỉ đáp ứng tốt nhu cầu của cư dân tại Quận 10 mà còn thu hút lượng lớn bệnh nhân từ các khu vực lân cận.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_202509268b021892c29a1d33cfa5df112e163f86.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Bệnh viện quận 10</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Đội ngũ y bác sĩ tại đây sở hữu trình độ chuyên môn vượt trội cùng nhiều năm kinh nghiệm làm việc tại các bệnh viện uy tín. Ngoài việc duy trì tác phong chuyên nghiệp, họ còn thấu hiểu tâm lý người bệnh, luôn tận tình chăm sóc và sẵn sàng hỗ trợ bất cứ khi nào cần thiết.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Địa chỉ: Số 571,&nbsp; Đường Sư Vạn Hạnh - P. 13, Q.10, TPHCM</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>5. Phòng khám nam khoa phường Hòa Hưng - Bệnh viện Trưng Vương</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bệnh viện Trưng Vương không chỉ là địa chỉ uy tín trong lĩnh vực sản phụ khoa và chăm sóc sức khỏe phụ nữ mà còn nổi bật ở nhiều chuyên khoa khác như chấn thương chỉnh hình, phẫu thuật thẩm mỹ, nội tiết, bệnh lý thận, nam khoa, và những lĩnh vực liên quan. Tất cả hoạt động tại đây đều hướng đến mục tiêu đảm bảo an toàn, đạt hiệu quả cao và nâng cao mức độ hài lòng của bệnh nhân.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_20250926cc41ad6f7ecf21e827eef4f818a86ca0.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Bệnh viện Trưng Vương</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bên cạnh đó, bệnh viện tự hào sở hữu đội ngũ y bác sĩ dày dạn kinh nghiệm, được đào tạo chuyên sâu, luôn tận tâm trong công tác khám chữa bệnh. Hệ thống trang thiết bị hiện đại, máy móc tiên tiến luôn được ưu tiên đầu tư, nâng cấp và kiểm định chất lượng định kỳ, nhằm đem lại kết quả chẩn đoán nhanh chóng, chính xác và đảm bảo an toàn tối đa cho mọi bệnh nhân.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Số 39,&nbsp; Đường Sư Vạn Hạnh, P.3, Quận 10, TPHCM</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>6. Phòng khám nam khoa phường Hòa Hưng - Đa khoa Việt Đức</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bệnh viện Đa khoa Việt Đức được xem là một trong những cơ sở y tế đáng tin cậy về nam khoa tại quận 10, chuyên cung cấp các dịch vụ khám và điều trị các vấn đề sức khỏe dành cho nam giới. Đội ngũ y bác sĩ tại đây luôn tận tâm và chu đáo, không ngừng cải thiện chất lượng phục vụ nhằm đảm bảo mang lại sự hài lòng tối đa cho bệnh nhân.&nbsp;&nbsp;</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_202509116120a4976be3dd8686a909fc5d043c6d.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Đa khoa Việt Đức</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Với chuyên môn cao, kiến thức sâu rộng và tác phong làm việc chuyên nghiệp, đội ngũ nhân viên y tế của bệnh viện nhận được nhiều đánh giá tích cực từ người bệnh. Quy trình khám chữa bệnh được tổ chức khoa học, tối ưu hóa sự tiện lợi và hiệu quả, đi kèm với sự hỗ trợ nhiệt tình từ các nhân viên phụ trách. Đáng chú ý, mức chi phí dịch vụ được thiết lập hợp lý, tạo cảm giác an tâm cho bệnh nhân khi quyết định điều trị tại đây.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Địa chỉ: Số 39,&nbsp; Đường Sư Vạn Hạnh, P.3, Quận 10, TPHCM</span>
+</p>
+<h2 style="text-align:justify;">
+<span style="background-color:transparent;color:#0000ff;">
+<strong>7. Phòng khám nam khoa phường Hòa Hưng - Phòng khám BS. Từ Thành Trí Dũng</strong>
+</span>
+</h2>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Bác sĩ Dũng được đông đảo nam giới đánh giá cao nhờ vào chuyên môn vững chắc, thái độ làm việc tận tâm, chu đáo và luôn giữ tinh thần vui vẻ trong quá trình chăm sóc bệnh nhân. Bên cạnh đó, phòng khám của bác sĩ còn là lựa chọn ưu tiên của nhiều người tại Quận 10, nổi bật hơn so với các bệnh viện nam khoa khác nhờ dịch vụ chăm sóc tận tình cùng đội ngũ nhân viên nhiệt huyết, luôn sẵn sàng hỗ trợ hết mình.</span>
+</p>
+<p style="text-align:center;">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091087c8216ad906ce2b236b750aa9a672f2.jpg">
+</p>
+<p style="text-align:center;">
+<span style="background-color:transparent;color:#000000;">
+<i>Phòng khám BS. Từ Thành Trí Dũng</i>
+</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phòng khám được trang bị hệ thống máy móc cùng trang thiết bị y tế tiên tiến, đảm bảo khả năng chẩn đoán chính xác và hỗ trợ điều trị hiệu quả. Ngoài ra, chi phí khám chữa bệnh tại đây được nhận xét là phải chăng, phù hợp với điều kiện kinh tế của nhiều đối tượng.</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">+ Địa chỉ: Số 271 – 273, Đ. Vĩnh Viễn, P.5, Quận 10, TP.HCM</span>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">Phường Hòa Hưng hiện có 7 phòng khám nam khoa chất lượng cao, được đông đảo người dân đánh giá cao về độ uy tín. Đây là những địa chỉ đáng tin cậy, nơi nam giới có thể yên tâm lựa chọn để chăm sóc sức khỏe và phòng tránh các vấn đề nam khoa phức tạp. Đặc biệt, nếu cần hỗ trợ hoặc giải đáp nhanh từ đội ngũ chuyên viên tư vấn, bạn chỉ cần sử dụng Khung Chat ngay bên dưới. Dịch vụ tư vấn miễn phí hoạt động 24/7, đảm bảo sự thuận tiện tối đa cho mọi người.</span>
+</p>
+<p style="text-align:center;">
+<a href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&amp;lng=en&amp;p=https://dakhoatanbinh.vn/&amp;r=&amp;rf1=&amp;rf2=&amp;cid=bf8407d2c2e943139eb1ec47a5acbfe0&amp;sid=8fddd1c01b8a40d8acba261ccc25cab7">
+<img src="https://sf-static.upanhlaylink.com/img/image_2025091067774f85d9944f1a3be28861e9689c12.jpg">
+</a>
+</p>
+<p style="text-align:justify;">
+<span style="background-color:transparent;color:#000000;">
+<strong>Xem thêm:&nbsp;</strong>
+</span>
+<a href="https://bvdkht.vn/feedback/2964/phong-kham-nam-khoa-quan-10">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Top 6 phòng khám nam khoa quận 10 khám chữa chất lượng với chi phí phải chăng</u>
+</strong>
+</span>
+</a>
+</p>
+<p style="text-align:justify;">
+<a href="http://bvdkht.vn/feedback/3081/phong-kham-nam-khoa-phuong-vuon-lai">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Top 7 phòng khám nam khoa phường Vườn Lài nổi tiếng về chất lượng dịch vụ y tế</u>
+</strong>
+</span>
+</a>
+</p>
+<p style="text-align:justify;">
+<a href="http://bvdkht.vn/feedback/3081/phong-kham-nam-khoa-phuong-dien-hong">
+<span style="background-color:transparent;color:#1155cc;">
+<strong>
+<u>Top 7 phòng khám nam khoa phường Diên Hồng đạt chuẩn y khoa</u>
+</strong>
+</span>
+</a>
+</p>
+
+
+
+
+   <div id="button" style="position:fixed; right:2%; bottom:10%; z-index:99999;">
+    <div class="chat">
+        <a target="_blank" href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&lng=en&p=https://dakhoatanbinh.vn/" rel="nofollow">
+            <img src="https://global-uploads.webflow.com/5c93193a199a684109dd80f8/600fe01e1663232bccf14af5_5d7f40f74cc8594c9d854185_rchat.gif" alt="Live Chat"> </a> </div> </div>
+            <a rel="follow,noopener" href="https://mfp.zoosnet.net/LR/Chatpre.aspx?id=MFP16752728&lng=en&p=https://dakhoatanbinh.vn/" class="w-inline-block"><div><img style="position:fixed;left:auto;top:40%;width:120px!important;right:0%;bottom:auto;margin-top:100px;margin-right:-30px;margin-bottom:0px;" src="https://global-uploads.webflow.com/640696c266a0cb0b83d2c96a/64094447e77edc0c1e47abba_5cd3943ffeabaf5f03705231_hotline.gif" rel="nofollow" alt="" width="120px;" class="image"></div></a>
+
+</body>
+</html>
+    `;
+
+}
+
+
